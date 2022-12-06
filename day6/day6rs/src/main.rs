abfbb725c3b43b2ebb763eecd492c42e20728430
David Estes <dav1do@users.noreply.github.com>
2022-12-06 12:08:31 -0700

    remove mutable set and instantiate from slice

--- day6/day6rs/src/main.rs
+++ day6/day6rs/src/main.rs
@@ -16,13 +16,9 @@ fn b() {
 fn doit(chars: usize) {
     let input = include_str!("./input.txt").chars().collect::<Vec<char>>();
     let mut pos = 0;
-    for (i, signal) in input.iter().enumerate() {
+    for (i, _signal) in input.iter().enumerate() {
         if i + chars - 1 < input.len() {
-            let mut set: HashSet<char> = HashSet::default();
-            set.insert(signal.clone());
-            for v in 0..chars {
-                set.insert(input[i + v]);
-            }
+            let set: HashSet<&char> = HashSet::from_iter(input[i..i + chars].into_iter());
             if set.len() == chars {
                 pos = i + chars; // want end of marker
                 break;
